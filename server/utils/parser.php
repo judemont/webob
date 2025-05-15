6a8762bc12d01d2e5533c3ae3336272baa9bdb35
JdM <julien@rmbi.ch>
2025-05-15 17:03:01 +0200

    remove url parameters

--- server/utils/parser.php
+++ server/utils/parser.php
@@ -7,17 +7,18 @@ function parseUrl($url) {
     $domain = $parsedUrl['host'];
     $domain = strtolower(trim($domain));
 
-    $path = $parsedUrl['path'];
-
+    // Extract the path and remove URL parameters
+    $path = isset($parsedUrl['path']) ? $parsedUrl['path'] : '';
     if (substr($path, -1) == "/") {
         $path = substr($path, 0, -1);
     }
     $path = strtolower(trim($path));
 
+    // Remove query string if present
+    $path = explode('?', $path)[0];
+
     $host = $parsedUrl['host'];
     $host = preg_replace('/^www\./', '', $host);
-
-
 
     return array('domain' => $domain, 'path' => $path);
 }
